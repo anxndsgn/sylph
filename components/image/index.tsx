e97aab3e6a9e9d1e9378bc4b04693cb8edc784d3
XIN <60823033+anxndsgn@users.noreply.github.com>
2024-11-26 00:01:57 +0800

    refactor image component and update works-at-zeabur post with new videos and WIP status

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -16,7 +16,11 @@ export default function MDXImage({ caption, alt, ...props }: MDXImageProps) {
   const href = props.src.toString();
 
   return (
-    <motion.a className="my-6 flex cursor-pointer flex-col justify-end gap-2" href={href} whileHover={{ scale: 0.975, opacity: 0.9 }}>
+    <motion.a
+      className="my-6 flex cursor-pointer flex-col justify-end gap-2"
+      href={href}
+      whileHover={{ scale: 0.975, opacity: 0.9 }}
+    >
       <div className="relative w-full overflow-hidden rounded-md">
         <Image
           unoptimized
